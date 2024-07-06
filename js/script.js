3b6beee6aa4b792c64c75ec25e387f28d114ee0f
Cositalindayo <163356103+Cositalindayo@users.noreply.github.com>
2024-07-06 00:38:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -137,7 +137,7 @@ function recargarPagina() {
 // Crear un elemento <script>
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '//pl21546160.profitablegatecpm.com/6c/9c/ca/6c9cca2344310fc5890c46b7c2e6c2ca.js';
+script.src = '//cessationhamster.com/23/d9/46/23d946bb84f00738562ba6c62609180c.js';
 
 // Agregar el elemento <script> al final del body
 document.body.appendChild(script);
